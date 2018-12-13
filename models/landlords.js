0cd80b892a77f9392305766e8125b429bc8582f2
Drew McGowan <michael.mcgowan@students.makeschool.com>
2018-12-12 20:01:02 -0800

    again

--- models/landlords.js
+++ models/landlords.js
@@ -1,78 +1,45 @@
-# RentMeAPI
-## ABOUT
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const Schema = mongoose.Schema;
 
-This API allows users to sign up to Host Rental Listings
+const LandlordSchema = new Schema({
+  createdAt       : { type: Date },
+  updatedAt       : { type: Date },
+  password        : { type: String, select: false, required: true },
+  username        : { type: String, required: true, unique: true },
+  rentals         : [{type: Schema.Types.ObjectId, ref: "Comment"}]
 
-Makes it easy for people to connect and find tenants
+});
 
-As this is a work in progress it still lacks valuable features such as searching for listings
+// Must use function here! ES6 => functions do not bind this!
+LandlordSchema.pre("save", function(next) {
+    //SET createdAt && updatedAt
+    const now = new Date();
+    this.updatedAt = now;
+    if (!this.createdAt) {
+        this.createdAt = now;
+    }
 
-Currently all the listings are in one place and that is the '/' route
+    // ENCRYPT Password
+    const user = this;
+    if (!user.isModified("password")) {
+        return next();
+    }
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            user.password = hash;
+            next();
+        });
+    });
+});
 
-This API currently can C.R.U.D listings and allows you to sign up for the app.
+LandlordSchema.methods.comparePassword = function(password, done) {
+    bcrypt.compare(password, this.password, (err, isMatch) => {
+        done(err, isMatch);
+    });
+};
 
-# Routes
-POST: Create new user requires a username and a password in the header
->/user/sign-up
 
-POST: Login as a user requires a username and a password in the header
->/user/login
 
-GET: Logout clears the cookie nToken
->/user/logout
 
-POST: new rental listing requires a neighborhood, zip code(int), type (room, apt etc) and price(int)
->/listing/rentals/view/new
-
-GET: Get a list of all listings
->/listing/rentals/
-
-GET: Gets a rental by the ID requires knowledge of the rental ID
->/listing/rentals/:id
-
-DELETE: deletes an entry by ID
-
->/listing/rentals/delete/:id
-
-PUT: Updates a listing by ID
->/listing.rentals/view/:id
-
-# Getting Started
-To get started install the dependencies in a new project folder.
-Since there is no front end on this API besides the docsify brochure you will either need to have knowledge of postman or go to the actual webpage on heroku to see the skinned project
-
-Github Pages link<br>
-https://ryanlsmith4.github.io/RentMeAPi/#/
-
-https://rentmenow.herokuapp.com/
-
-## Dependencies
-I have used node and npm for all the dependencies
-To install these in your project folder type
-```npm i <package> --save```
-
-    "bcrypt": "^3.0.2",
-    "bootstrap": "^4.1.3",
-    "chai": "^4.2.0",
-    "chai-http": "^4.2.0",
-    "cookie-parser": "^1.4.3",
-    "dotenv": "^6.2.0",
-    "express": "^4.16.4",
-    "express-handlebars": "^3.0.0",
-    "jquery": "^3.0.0",
-    "jsonwebtoken": "^8.3.0",
-    "method-override": "^3.0.0",
-    "mongoose": "^5.3.4",
-    "popper.js": "^1.14.4"
-
-# TODO:
-1. add rating and payment system <br>
-2. incorporate search function <br>
-3. associate Landlord with rentals <br>
-4. add chat feature <br>
-5. add lease upload system <br>
-6. incorporate trending properties(properties that have been looked at multiple times)
-7. Add a filter to the search results
-
-# Contact
-if you have question or comments or want to contribut the github link in the top right corner will take you to the repo!
+module.exports = mongoose.model('Landlord', LandlordSchema);
